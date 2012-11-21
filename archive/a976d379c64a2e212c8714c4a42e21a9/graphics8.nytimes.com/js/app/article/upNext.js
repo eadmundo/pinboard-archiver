28a39fdd0ffc5520d2db7dea012ff615780b1605
Edward Stone <edward.stone@tobias.tv>
2012-11-21 16:57:13 +0000

    removed archive files

--- archive/a976d379c64a2e212c8714c4a42e21a9/graphics8.nytimes.com/js/app/article/upNext.js
+++
@@ -1,6 +0,0 @@
-/* $Id: upNext.js 41403 2010-07-14 20:55:56Z joseph.williams $ */
-
-var NYTD = NYTD || {};
-
-NYTD.require('/js/app/lib/NYTD/jsonpfeed/jsonp-feed.js');
-NYTD.require('/js/app/article/upNextController.js');
--- archive/a976d379c64a2e212c8714c4a42e21a9/js.nyt.com/js/app/moth/moth.js
+++
@@ -1,4 +0,0 @@
-NYTD.require('/js/app/lib/scriptaculous/extensions/effect_scroll.js');
-NYTD.require('/js/app/lib/iscroll/3.6.min.js');
-NYTD.require('/js/app/moth/mothController.js');
-NYTD.require('/js/app/moth/mothLegacy.js');
